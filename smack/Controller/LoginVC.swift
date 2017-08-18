1b3707933db03bd6dbb79857c2ab43d3a7626189
Andrew Greenough <andygreenough@gmail.com>
2017-08-18 15:24:58 +0100

    LoginVC UI

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Andrew Greenough on 18/08/2017.
+//  Copyright © 2017 Andrew Greenough. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Andrew Greenough on 18/08/2017.
+//  Copyright © 2017 Andrew Greenough. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
