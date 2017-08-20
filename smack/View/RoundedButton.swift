ef1546403416c8b1e538373fcd15932376510546
Andrew Greenough <andygreenough@gmail.com>
2017-08-20 10:51:20 +0100

    Registering a User

---
+++ smack/View/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  smack
+//
+//  Created by Andrew Greenough on 20/08/2017.
+//  Copyright © 2017 Andrew Greenough. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
