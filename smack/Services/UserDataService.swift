7c8e1b2c010bf4115d558140ce667a38379ec8a6
Andrew Greenough <andygreenough@gmail.com>
2017-08-20 13:53:50 +0100

    Creating a user

---
+++ smack/Services/UserDataService.swift
@@ -0,0 +1,32 @@
+//
+//  UserDataService.swift
+//  smack
+//
+//  Created by Andrew Greenough on 20/08/2017.
+//  Copyright © 2017 Andrew Greenough. All rights reserved.
+//
+
+import Foundation
+
+class UserDataService{
+    
+    static let instance = UserDataService()
+    
+    private(set) var id = ""
+    private(set) var avatarColor = ""
+    private(set) var avatarName = ""
+    private(set) var email = ""
+    private(set) var name = ""
+    
+    func setUserData(id: String, color: String, avatarName: String, email: String, name: String) {
+        self.id = id
+        self.avatarColor = color
+        self.avatarName = avatarName
+        self.email = email
+        self.name = name
+    }
+    
+    func setAvatarName(avatarName: String) {
+        self.avatarName = avatarName
+    }
+}
