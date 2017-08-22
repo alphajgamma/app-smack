cdbdc36215f4510015c7e94775911c836fa37f4f
Andrew Greenough <andygreenough@gmail.com>
2017-08-22 11:31:00 +0100

    Logging in users

--- smack/Controller/LoginVC.swift
+++ smack/Controller/LoginVC.swift
@@ -10,10 +10,37 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+    
 
-        // Do any additional setup after loading the view.
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = userNameTxt.text , userNameTxt.text != "" else {return}
+        guard let pass = passwordTxt.text , passwordTxt.text != "" else {return}
+        
+        AuthService.instance.loginUser(email: email, password: pass) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                        self.spinner.isHidden = true
+                        self.spinner.stopAnimating()
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                })
+            }
+        }
     }
     
     @IBAction func closePressed(_ sender: Any) {
@@ -22,5 +49,11 @@ class LoginVC: UIViewController {
     
     @IBAction func createAccountBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+    }
+    
+    func setupView(){
+        spinner.isHidden = true
+        userNameTxt.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedStringKey.foregroundColor:smackPurplePlaceholder])
+        passwordTxt.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedStringKey.foregroundColor:smackPurplePlaceholder])
     }
 }
