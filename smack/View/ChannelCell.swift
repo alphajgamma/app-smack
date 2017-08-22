1ddac96d2e7b5f455cfff996c7bd8947ba025f78
Andrew Greenough <andygreenough@gmail.com>
2017-08-22 15:14:35 +0100

    Channels TableView

---
+++ smack/View/ChannelCell.swift
@@ -0,0 +1,36 @@
+//
+//  ChannelCell.swift
+//  smack
+//
+//  Created by Andrew Greenough on 22/08/2017.
+//  Copyright © 2017 Andrew Greenough. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+
+    // Outlets
+    @IBOutlet weak var channelName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+    
+    func configureCell(channel: Channel){
+        let title = channel.channelTitle ?? ""
+        channelName.text = "#\(title)"
+    }
+
+}
