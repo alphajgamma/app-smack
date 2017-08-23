55222a3a77ea1dbf546035f13308938750d4cb52
Andrew Greenough <andygreenough@gmail.com>
2017-08-23 16:48:36 +0100

    Displaying chat messages

---
+++ smack/View/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  smack
+//
+//  Created by Andrew Greenough on 23/08/2017.
+//  Copyright © 2017 Andrew Greenough. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    // Outlets
+    @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timeStampLbl: UILabel!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configureCell(message: Message) {
+        messageBodyLbl.text = message.message
+        userNameLbl.text = message.userName
+        userImg.image = UIImage(named: message.userAvatar)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+
+}
