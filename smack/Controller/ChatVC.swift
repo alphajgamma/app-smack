104bea180ac03c1e6d1da996029eec552432b7e8
Andrew Greenough <andygreenough@gmail.com>
2017-08-23 15:37:55 +0100

    Fetching messages

--- smack/Controller/ChatVC.swift
+++ smack/Controller/ChatVC.swift
@@ -46,11 +46,26 @@ class ChatVC: UIViewController {
     func updateWithChannel() {
         let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
         channelNameLbl.text = "#\(channelName)"
+        getMessages()
     }
     
     func onLoginGetMessages() {
         MessageService.instance.findAllChannel { (success) in
-            // Do stuff with channels
+            if success {
+                if MessageService.instance.channels.count > 0 {
+                    MessageService.instance.selectedChannel = MessageService.instance.channels[0]
+                    self.updateWithChannel()
+                } else {
+                    self.channelNameLbl.text = "No chanels yet!"
+                }
+            }
+        }
+    }
+    
+    func getMessages() {
+        guard let channelId = MessageService.instance.selectedChannel?.id else { return }
+        MessageService.instance.findAllMessagesForChannel(channelId: channelId) { (success) in
+            
         }
     }
 }
---
+++ smack/Model/Message.swift
@@ -0,0 +1,20 @@
+//
+//  Message.swift
+//  smack
+//
+//  Created by Andrew Greenough on 23/08/2017.
+//  Copyright © 2017 Andrew Greenough. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    private(set) var message: String!
+    private(set) var userName: String!
+    private(set) var channelId: String!
+    private(set) var userAvatar: String!
+    private(set) var userAvatarColor: String!
+    private(set) var id: String!
+    private(set) var timeStamp: String!
+}
+
